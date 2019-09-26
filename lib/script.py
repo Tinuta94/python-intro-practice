388d40777c10ad719383be46c24026445f162d55
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-09-26 07:46:50 -0400

    hw

---
+++ lib/script.py
@@ -0,0 +1,23 @@
+from capitals import states
+
+#class states(object):
+def __init__(self, name, capital) :
+    self.name = name
+    self.capital = capital
+
+x = len(states)
+print(x)
+
+#states = dict_key, dict_values
+print (states[3])
+
+#def game()
+#print(states[3].name)
+#answer = input('Enter the corresponding capital')
+#if answer == states[3].capital:
+    #return correct
+#else:
+ #   return incorrect
+
+
+print(states)
