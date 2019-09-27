dd5d8b19d83357977ec76e462ed308ff65175ea9
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-09-26 21:32:59 -0400

    hw

--- lib/script.py
+++ lib/script.py
@@ -1,23 +1,29 @@
 from capitals import states
-
-#class states(object):
-def __init__(self, name, capital) :
-    self.name = name
-    self.capital = capital
-
-x = len(states)
-print(x)
-
-#states = dict_key, dict_values
-print (states[3])
-
-#def game()
-#print(states[3].name)
-#answer = input('Enter the corresponding capital')
-#if answer == states[3].capital:
-    #return correct
-#else:
- #   return incorrect
-
-
-print(states)
+import random
+​
+print ("Welcome, ready to test your knowledge of state capitals?")
+​
+random.shuffle(states)
+​
+for state in states:
+    state["Correct"] = 0
+    state["Incorrect"] = 0
+​
+def play():
+    for state in states:
+        print ("What is the capital of {}?".format(state['name']))
+        if input() == state["capital"]:
+            print('Correct!')
+            state["Correct"] += 1
+        else:
+            print("Wrong!")
+            state["Incorrect"] += 1
+        print("Correct: {}".format(state["Correct"]))
+        print("Incorrect: {}".format(state["Incorrect"]))
+    print ("Would you like to play again? y/n")
+    if input() == "y":
+        play()
+    else:
+        None
+​
+play()
